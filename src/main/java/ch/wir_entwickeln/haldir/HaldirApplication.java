884eb81d72247c9beefbf27b62961ed5306a52cc
Ronny Stauffer <ronny.stauffer@wir-entwickeln.ch>
2016-08-01 17:51:21 +0200

    Initial import

---
+++ src/main/java/ch/wir_entwickeln/haldir/HaldirApplication.java
@@ -0,0 +1,12 @@
+package ch.wir_entwickeln.haldir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaldirApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HaldirApplication.class, args);
+	}
+}
---
+++ src/test/java/ch/wir_entwickeln/haldir/HaldirApplicationTests.java
@@ -0,0 +1,16 @@
+package ch.wir_entwickeln.haldir;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HaldirApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
