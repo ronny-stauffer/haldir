6b39f921b6d6c23ff9b260887e68046c58a83c09
Ronny Stauffer <ronny.stauffer@wir-entwickeln.ch>
2016-08-02 21:26:59 +0200

    First implementation

--- src/main/java/ch/wir_entwickeln/haldir/HaldirApplication.java
+++ src/main/java/ch/wir_entwickeln/haldir/HaldirApplication.java
@@ -2,10 +2,11 @@ package ch.wir_entwickeln.haldir;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class HaldirApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(HaldirApplication.class, args);
 	}
